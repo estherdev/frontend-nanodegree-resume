9de59554fbdedaf6bb9de0874eb910839659a43b
estherdev <esther.kuforiji@faststream.civilservice.gov.uk>
2015-09-28 15:10:00 +0100

    Created variables for name and roledefined, replaced placeholders and prepended replacements to the page.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,11 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Esther");
+var name = "Esther";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
